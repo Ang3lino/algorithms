f8cb15e80d045c6817510fb43dd186516cd7db50
Angel Lopez Manriquez <sigma271@outlook.com>
2020-05-12 21:45:26 -0500

    Constructor

--- wordnet/src/main/java/WordNet.java
+++ wordnet/src/main/java/WordNet.java
@@ -2,20 +2,48 @@
 
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.ResizingArrayBag;
 import edu.princeton.cs.algs4.StdOut;
 
+import javax.swing.plaf.basic.BasicBorders.SplitPaneBorder;
+
+import edu.princeton.cs.algs4.Bag;
 
 public class WordNet {
     private Digraph digraph;
-    private Bag<String> intToString;    
+    private String[] intToString;
+
+    private int buildIntToString(String synsets) {
+        In in = new In(synsets); // reader
+        Queue<String> lines = new Queue<>();
+        while (in.hasNextLine()) {
+            lines.enqueue(in.readLine());
+        }
+        int vertexCount = lines.size();
+        intToString = new String[vertexCount];
+        for (int i = 0; !lines.isEmpty(); ++i) {
+            intToString[i] = lines.dequeue();
+        }
+        return vertexCount;
+    }
+
+    private void buildDigraph(String hypernyms, int V) {
+        digraph = new Digraph(V);
+        for (In in = new In(hypernyms); in.hasNextLine(); ) {
+            String[] splitted = in.readLine().split(",");
+            Integer[] vertices = new Integer[splitted.length];
+            for (int i = 0; i < vertices.length; ++i) 
+                vertices[i] = Integer.parseInt(splitted[i]);
+            for (int i = 1; i < vertices.length; ++i) 
+                digraph.addEdge(vertices[0], vertices[i]);
+        }
+    }
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
-        In in = new In(synsets);
-        int vertexCount = 0;
-        while (in.hasNextLine()) {
-            StdOut.println(in.readLine());
-        }
+        int verticesCount = buildIntToString(synsets);
+        buildDigraph(hypernyms, verticesCount);
     }
 
     public static void main(String[] args) {
